69a7bcf18201237bcea98d60bcddb665c12c2a1c
Rifat Mamayusupov <rifatmamayusupov92@gmail.com>
2023-02-18 19:19:53 +0500

    second_format_json

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import json
 
 # empty dict o'zgaruvchi ochish
 data_dict = {}
-
+data_list=[]
 # example uchun 10ta linkdan data
 text_data = []
 print("Ma'lumot yozish boshlandi......")
@@ -18,14 +18,19 @@ for url in a_title_link[:10]:
 # dict o'zgaruvchisiga ma'lumotlarni qo'shish
 for i in range(len(text_data)):
     if url is not None and img_data_links[i] is not None:
-       data_dict[title_name_data[i]] = [url+img_data_links[i], text_data[i]]
-
+       data_dict = {
+           'id' : i,
+           "title":title_name_data[i],
+            "img_link": url+img_data_links[i],
+                                'discription': text_data[i]}
+    data_list.append(data_dict)
+# [""url+img_data_links[i], text_data[i]]
 # dict fileni jsonga o'zgartirish
-json_data = json.dumps(data_dict, ensure_ascii=False, indent=6)
+json_data = json.dumps(data_list, ensure_ascii=False, indent=6)
 
 
 # json fileni yozish
-with open('jsondata.json', 'w', encoding='utf-8') as f:
+with open('jsondata2.json', 'w', encoding='utf-8') as f:
     f.write(json_data)
 
     print('Jaroayon tugadi.....')
